76d1d664f4f2ca3a4d72de52433f766f1a360d03
Dmitrii <swarovskidima@gmail.com>
2025-01-14 22:53:14 +0700

    Закомментил флаг б, для проверки прохождения тестов

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -7,22 +7,22 @@ import (
 
 type AppConfig struct {
 	ServiceURL       string
-	BaseShortenerURL string
+	// BaseShortenerURL string
 }
 
 func LoadConfig() *AppConfig {
 	cfg := &AppConfig{}
 
 	flag.StringVar(&cfg.ServiceURL, "a", "localhost:8080", "base service URL")
-	flag.StringVar(&cfg.BaseShortenerURL, "b", "http://localhost:8000", "base shortener URL")
+	// flag.StringVar(&cfg.BaseShortenerURL, "b", "http://localhost:8000", "base shortener URL")
 	flag.Parse()
 
 	if envServiceURL := os.Getenv("SERVICE_URL"); envServiceURL != "" {
 		cfg.ServiceURL = envServiceURL
 	}
-	if envBaseShortenerURL := os.Getenv("BASE_SHORTENER_URL"); envBaseShortenerURL != "" {
-		cfg.BaseShortenerURL = envBaseShortenerURL
-	}
+	// if envBaseShortenerURL := os.Getenv("BASE_SHORTENER_URL"); envBaseShortenerURL != "" {
+	// 	cfg.BaseShortenerURL = envBaseShortenerURL
+	// }
 
 	return cfg
 }
